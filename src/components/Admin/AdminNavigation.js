9d17bb2991ed38bb13158b2c102cdd5c5506e3fb
Mayank Jha <mjha199402@gmail.com>
2024-02-19 14:37:26 +0530

    Mobile responsiveness improved

--- src/components/Admin/AdminNavigation.js
+++ src/components/Admin/AdminNavigation.js
@@ -1,17 +1,11 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-const AdminNavigation = (props)=>{
-
-    const [showNavigation, setShowNavigation] = useState(false);
-
-    const toggleNavigation = ()=>{
-        setShowNavigation(prevState => !prevState);
-    }
+const AdminNavigation = ({showNavigation, toggleNavigation})=>{
 
     return (
-        <div className={`fixed left-0 top-[100px] w-[100%] h-[100dvh] ${showNavigation ? 'bg-[#adadadbd] overflow-hidden' : ''}`}>
-        <div onClick={toggleNavigation} id="hamburgerBtn" className={`hamburgerBtn ml-6 text-6xl cursor-pointer text-[#f05941] hover:text-[#1c1c1cd9]`}><i className='bx bx-right-arrow-alt'></i></div>
+        <div className={`fixed left-0 top-[100px] w-[100%] h-[100dvh] ${showNavigation ? 'bg-[#adadadbd] overflow-hidden z-10' : 'z-[-1]'}`}>
+        {/* <div onClick={toggleNavigation} id="hamburgerBtn" className={`hamburgerBtn ml-6 text-6xl cursor-pointer text-[#f05941] hover:text-[#1c1c1cd9]`}><i className='bx bx-right-arrow-alt '></i></div> */}
 
         <div className={`nav-section h-[inherit] w-[60%] py-10 px-4 bg-white text-[#1c1c1cd9] border-b-[1px] border-t-[1px] border-r-[1px] border-[#e3e3e3] block absolute top-0 left-0 ${showNavigation ? 'block' : 'hidden'}`}>
 
@@ -20,7 +14,7 @@ const AdminNavigation = (props)=>{
                     <div onClick={toggleNavigation} className="text-6xl cursor-pointer text-[#f05941] font-extralight mt-[-8px] hover:text-[#1c1c1cd9"><i className='bx bx-left-arrow-alt'></i></div>
                 </div>
 
-                <div className="mt-5">
+                <div className="mt-10">
                     <ul>
                         <li className="cursor-pointer px-2 text-4xl my-4 border-b-[1px] border-[#d6d0d0] font-light hover:text-[#f05941] pb-2"><i className='bx bxs-dashboard mr-2'></i><span><Link to="/admin">Dashboard</Link></span></li>
 
--- src/components/Admin/AdminPanel.js
+++ src/components/Admin/AdminPanel.js
@@ -1,14 +1,21 @@
 import {Outlet} from 'react-router-dom';
-
+import { useState } from 'react';
 import AdminNavigation from "./AdminNavigation";
 
 const AdminPanel = ()=>{
 
+    const [showNavigation, setShowNavigation] = useState(false);
+
+    const toggleNavigation = ()=>{
+        setShowNavigation(prevState => !prevState);
+    }
+
     return (
 
         <div className="flex-1 admin-container flex relative">
-                        <AdminNavigation />
+                        <AdminNavigation showNavigation={showNavigation} toggleNavigation={toggleNavigation} />
                         <div className="w-[-webkit-fill-available] ml-3">
+                            <div onClick={toggleNavigation} id="hamburgerBtn" className={`hamburgerBtn ml-6 text-6xl cursor-pointer text-[#f05941] hover:text-[#1c1c1cd9]`}><i className='bx bx-right-arrow-alt '></i></div>
                             <Outlet></Outlet>
                         </div>
         </div>
